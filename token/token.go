736ca96d5c2e51ab55c9c9902f71c16506523a26
Aleksei Bingham <asb9189@rit.edu>
2021-05-21 00:15:35 -0400

    Enhancing the lexer to support a larger subset of monkeylang.

--- token/token.go
+++ token/token.go
@@ -28,8 +28,19 @@ const (
 	LET      = "LET"
 )
 
-type TokenType string
+var keywords = map[string]TokenType {
+	"fn": FUNCTION,
+	"let": LET,
+}
 
+func LookupIdent(ident string) TokenType {
+	if tok, ok := keywords[ident]; ok {
+		return tok
+	}
+	return IDENT
+}
+
+type TokenType string
 type Token struct {
 	Type    TokenType
 	Literal string
