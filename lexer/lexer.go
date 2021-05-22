5df42a3adb0db53450528a944546ff0b8ce00cda
Aleksei Bingham <asb9189@rit.edu>
2021-05-21 21:11:22 -0400

    Extended the lexer with greater support for more token types

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -32,7 +32,14 @@ func (l *Lexer) NextToken() token.Token {
 
   switch l.ch {
     case '=':
-      tok = newToken(token.ASSIGN, l.ch)
+      if l.peekChar() == '=' {
+        ch := l.ch
+        l.readChar()
+        literal := string(ch) + string(l.ch)
+        tok = token.Token{Type: token.EQ, Literal: literal}
+      } else {
+        tok = newToken(token.ASSIGN, l.ch)
+      }
     case ';':
       tok = newToken(token.SEMICOLON, l.ch)
     case '(':
@@ -43,6 +50,25 @@ func (l *Lexer) NextToken() token.Token {
       tok = newToken(token.COMMA, l.ch)
     case '+':
       tok = newToken(token.PLUS, l.ch)
+    case '-':
+      tok = newToken(token.MINUS, l.ch)
+    case '!':
+      if l.peekChar() == '=' {
+        ch := l.ch
+        l.readChar()
+        literal := string(ch) + string(l.ch)
+        tok = token.Token{Type: token.NOT_EQ, Literal: literal}
+      } else {
+        tok = newToken(token.BANG, l.ch)
+      }
+    case '/':
+      tok = newToken(token.SLASH, l.ch)
+    case '*':
+      tok = newToken(token.ASTERISK, l.ch)
+    case '<':
+      tok = newToken(token.LT, l.ch)
+    case '>':
+      tok = newToken(token.GT, l.ch)
     case '{':
       tok = newToken(token.LBRACE, l.ch)
     case '}':
@@ -65,6 +91,13 @@ func (l *Lexer) NextToken() token.Token {
   }
   l.readChar()
   return tok
+}
+
+func (l *Lexer) peekChar() byte {
+  if l.readPosition >= len(l.input) {
+    return 0
+  }
+  return l.input[l.readPosition]
 }
 
 func (l *Lexer) readNumber() string {
--- token/token.go
+++ token/token.go
@@ -13,6 +13,16 @@ const (
 	// Operators
 	ASSIGN = "="
 	PLUS   = "+"
+	MINUS = "-"
+	BANG = "!"
+	ASTERISK = "*"
+	SLASH = "/"
+
+	LT = "<"
+	GT = ">"
+
+	EQ = "=="
+	NOT_EQ = "!="
 
 	// Delimiters
 	COMMA     = ","
@@ -26,11 +36,21 @@ const (
 	// Keywords
 	FUNCTION = "FUNCTION"
 	LET      = "LET"
+	TRUE = "TRUE"
+	FALSE = "FALSE"
+	IF = "IF"
+	ELSE = "ELSE"
+	RETURN = "RETURN"
 )
 
 var keywords = map[string]TokenType {
 	"fn": FUNCTION,
 	"let": LET,
+	"true": TRUE,
+	"false": FALSE,
+	"if": IF,
+	"else": ELSE,
+	"return": RETURN,
 }
 
 func LookupIdent(ident string) TokenType {
