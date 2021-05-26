8c069fc6d533d20bfd50bdc4430fa6d58c207c6c
Aleksei Bingham <asb9189@rit.edu>
2021-05-25 22:52:46 -0400

    Added String() for all AST nodes + Program struct and added a unit test

--- ast/ast.go
+++ ast/ast.go
@@ -6,6 +6,7 @@ import (
 
 type Node interface {
   TokenLiteral() string
+  String() string
 }
 
 type Statement interface {
@@ -21,6 +22,15 @@ type Expression interface {
 // root node of each AST the parser produces
 type Program struct {
   Statements []Statement
+}
+
+func (p *Program) String() string {
+  var out = bytes.Buffer
+
+  for _, s := range p.Statements {
+    out.WriteString(s.String())
+  }
+  return out.String()
 }
 
 func (p *Program) TokenLiteral() string {
@@ -40,6 +50,21 @@ type LetStatement struct {
 func (ls *LetStatement) statementNode() {}
 func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal }
 
+func (ls *LetStatement) String() string {
+  var out = bytes.Buffer
+
+  out.WriteString(ls.TokenLiteral() + " ")
+  out.WriteString(ls.Name.String())
+  out.WriteString(" = ")
+  if ls.Value != nil {
+    out.WriteString(ls.Value.String())
+  }
+
+  out.WriteString(";")
+
+  return out.String()
+}
+
 type ReturnStatement struct {
   Token token.Token // the 'return' token
   ReturnValue Expression
@@ -47,6 +72,19 @@ type ReturnStatement struct {
 
 func (rs *ReturnStatement) statementNode() {}
 func (rs *ReturnStatement) TokenLiteral() string { return rs.Token.Literal }
+func (rs *ReturnStatement) String() string {
+  var out = bytes.Buffer
+
+  out.WriteString(rs.TokenLiteral() + " ")
+  if rs.ReturnValue != nil {
+    out.WriteString(rs.ReturnValue.String())
+  }
+
+  out.WriteString(";")
+
+  return out.String()
+
+}
 
 type Identifier struct {
   Token token.Token // the token.IDENT token
@@ -55,3 +93,18 @@ type Identifier struct {
 
 func (i *Identifier) expressionNode() {}
 func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
+func (i *Identifier) String() string { return i.Value }
+
+type ExpressionStatement struct {
+  Token token.Token // the first token of the expression
+  Expression Expression
+}
+
+func (es *ExpressionStatement) statementNode() {}
+func (es *ExpressionStatement) TokenLiteral() string { return es.Token.Literal }
+func (es *ExpressionStatement) String() string {
+  if es.Expression != nil {
+    return es.Expression.String()
+  }
+  return ""
+}
