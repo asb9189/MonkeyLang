4a1e5e787bca709523625f7e92120aa7120d8aee
Aleksei Bingham <asb9189@rit.edu>
2021-05-30 11:50:08 -0400

    Added support for parsing Integer literals

--- parser/parser.go
+++ parser/parser.go
@@ -5,6 +5,7 @@ import (
   "MonkeyLang/lexer"
   "MonkeyLang/token"
   "fmt"
+  "strconv"
 )
 
 const (
@@ -42,6 +43,7 @@ func New(l *lexer.Lexer) *Parser {
 
   p.prefixParseFns = make(map[token.TokenType]prefixParseFn)
   p.registerPrefix(token.IDENT, p.parseIdentifier)
+  p.registerPrefix(token.INT, p.parseIntegerLiteral)
 
   // Read two tokens, so curToken and peekToken are both set
   p.nextToken()
@@ -176,4 +178,18 @@ func (p *Parser) expectPeek(t token.TokenType) bool {
 
 func (p *Parser) parseIdentifier() ast.Expression {
   return &ast.Identifier{Token: p.curToken, Value: p.curToken.Literal}
+}
+
+func (p *Parser) parseIntegerLiteral() ast.Expression {
+  lit := &ast.IntegerLiteral{Token: p.curToken}
+
+  value, err := strconv.ParseInt(p.curToken.Literal, 0, 64)
+  if err != nil {
+    msg := fmt.Sprintf("could not parse %q as integer", p.curToken.Literal)
+    p.errors = append(p.errors, msg)
+    return nil
+  }
+
+  lit.Value = value
+  return lit
 }
