c9c533dc60612a46273826a5efee41aea4b74995
Aleksei Bingham <asb9189@rit.edu>
2021-06-05 17:10:38 -0400

    Implemented function literal parsing

--- ast/ast.go
+++ ast/ast.go
@@ -2,6 +2,7 @@ package ast
 
 import (
   "MonkeyLang/token"
+  "strings"
   "bytes"
 )
 
@@ -193,7 +194,7 @@ func (ie *IfExpression) String() string {
 }
 
 type BlockStatement struct {
-  Token token.Token // the '{' token
+  Token token.Token // The '{' token
   Statements []Statement
 }
 
@@ -205,5 +206,30 @@ func (bs *BlockStatement) String() string {
   for _, s := range bs.Statements {
     out.WriteString(s.String())
   }
+  return out.String()
+}
+
+type FunctionLiteral struct {
+  Token token.Token // The 'fn' token
+  Parameters []*Identifier
+  Body *BlockStatement
+}
+
+func (fl *FunctionLiteral) expressionNode() {}
+func (fl *FunctionLiteral) TokenLiteral() string { return fl.Token.Literal }
+func (fl *FunctionLiteral) String() string {
+  var out bytes.Buffer
+
+  params := []string{}
+  for _, p := range fl.Parameters {
+    params = append(params, p.String())
+  }
+
+  out.WriteString(fl.TokenLiteral())
+  out.WriteString("(")
+  out.WriteString(strings.Join(params, ", "))
+  out.WriteString(") ")
+  out.WriteString(fl.Body.String())
+
   return out.String()
 }
